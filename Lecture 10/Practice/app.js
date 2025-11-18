012b61f144b3eb872c2918e79ef89a36a80b8b49
Anurag0120 <anuragilake727@gmail.com>
2025-11-18 22:54:52 +0530

    Changes in lecture 10 practice set

--- Lecture 10/Practice/app.js
+++ Lecture 10/Practice/app.js
@@ -1,7 +1,7 @@
 const express =  require('express');
 const bodyParser =  require('body-parser');
 
-const app =express();
+const app = express();
 
 app.use((req, res, next) => {
   console.log("first mid", req.url, req.method);
